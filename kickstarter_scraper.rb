781cf9b85960c085865811a4503488ed07d767da
Chaim Bryski <luzybry94@gmail.com>
2020-09-02 18:24:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,10 +3,15 @@ require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
   
   projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
  
-  kickstarter = Nokogiri::HTML(html)
+  
 end
 
 create_project_hash
